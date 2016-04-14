2d798a391c5d22e7dd9b38c159db1dbfb5dbcf18
Li Qi <crazymage@pku.edu.cn>
2016-04-14 20:14:38 +0800

    update

--- run_word2vec.sh
+++ run_word2vec.sh
@@ -1,1 +1,1 @@
-./word2vec -train corpus.txt -output vectors.txt -cbow 0 -size 50 -window 10 -negative 5 -hs 0 -sample 1e-5 -threads 3 -binary 0 -min-count 5 -save-vocab vocab.txt
+nohup ./word2vec -train ~/corpus/enwiki-20160305-pages-articles.txt -output word2vec50.wiki.txt -cbow 0 -size 50 -window 10 -negative 5 -hs 0 -sample 1e-5 -threads 4 -binary 0 -min-count 512 -save-vocab vocab.txt &
